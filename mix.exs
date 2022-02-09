dcb3768baaeaf131e7e5264ebe458909dcb3472a
FCO-1 <pacomendez012@gmail.com>
2022-02-09 09:44:43 -0600

    cambio de mix exs

--- mix.exs
+++ mix.exs
@@ -65,8 +65,8 @@ defmodule ComsumeDeps.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "phx.digest": ["phx.digest"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      "phoenix.digest": ["phx.digest"]
     ]
   end
 end
